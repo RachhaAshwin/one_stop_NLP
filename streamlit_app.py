a0bcb10d59e060ff6c15e927a4de3c411c5a0abe
RachhaAshwin <99497220+RachhaAshwin@users.noreply.github.com>
2022-03-10 21:39:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ st.sidebar.write('')
 #CONTACT
 ########
 expander = st.sidebar.expander('Contact')
-expander.write("I would love for your feedback, contact me on linkedin [LinkedIn] (https://www.linkedin.com/in/ashwinrachha/))
+expander.write("I would love for your feedback, contact me on linkedin [LinkedIn] (https://www.linkedin.com/in/ashwinrachha/)")
                
 
 #######
